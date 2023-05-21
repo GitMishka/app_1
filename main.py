75becd268788c0ac6e9b6bfb6b36f71f3e555a6c
Misha <mishagrigoryan@gmail.com>
2023-05-21 15:06:16 -0400

    goverment api

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ from db.db_utils import connect_db, insert_data_into_db
 from services.data_service import fetch_data_from_api
 
 def main():
-
+    # Replace these with your AWS RDS details
     host = "your_aws_rds_host"
     dbname = "your_db_name"
     user = "your_username"
     password = "your_password"
     port = "your_port_number"
 
-    # API url you want to fetch data from
-    api_url = ""
+    # Replace these with your API details
+    api_url = "your_api_url"
+    api_key = "your_api_key"
 
     conn = connect_db(host, dbname, user, password, port)
     if conn is not None:
-        data = fetch_data_from_api(api_url)
+        data = fetch_data_from_api(api_url, api_key)
         if data is not None:
             insert_data_into_db(conn, data)
         conn.close()
--- services/data_services.py
+++ services/data_services.py
@@ -1,8 +1,9 @@
 import requests
 import json
 
-def fetch_data_from_api(api_url):
-    response = requests.get(api_url)
+def fetch_data_from_api(api_url, api_key):
+    headers = {'Authorization': 'Bearer ' + api_key}
+    response = requests.get(api_url, headers=headers)
     if response.status_code == 200:
         return json.loads(response.text)
     else:
